b375bfb065bd7f2822ea6b781dac565028c75b53
Youssef Fadel <youssef_a_fadel@hotmail.com>
2020-03-02 15:20:19 +0200

    Added additional keyword encryption/decryption

--- Encryptor/src/MainClass.java
+++ Encryptor/src/MainClass.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class MainClass {
     static Encryptor enc = new Encryptor();
-    //static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
+        
+        //The user menu interface
         Scanner scan = new Scanner(System.in);
         int value = 0;
-        while(value < 5 && value >= 0) {
-            System.out.println("\n~Main Menu:\n1.Encrypt\n2.Decrypt\n3.Modify number of shifts (current: " + enc.getN() + ")\n4.Modify value of each shift (current: " + enc.getS() + ")\n5.Exit\n");
+        while(value < 7 && value >= 0) {
+            System.out.println("\n~Main Menu:\n1.Encrypt\n2.Decrypt\n3.Modify number of shifts (current: " + enc.getN() + ")\n4.Modify value of each shift (current: " + enc.getS() + ")\n5.Encrypt using a keyword\n6.Decrypt using a keyword\n7.Exit\n");
             value = scan.nextInt();
             switch(value) {
                 case(1):
@@ -22,16 +23,25 @@ public class MainClass {
                 case(4):
                     case4();
                 break;
+                case(5):
+                    case5();
+                break;
+                case(6):
+                    case6();
+                break;
             }
         }
     }
-
+    
+    
+    //Each option displayed according to selection
+    
     public static void case1() {
         Scanner scan = new Scanner(System.in);
         System.out.print("\n~Enter text to encrypt: ");
         String x=  scan.nextLine();
         enc.setInput(x);
-        System.out.println("Encryption: " + enc.encrypt(1));
+        System.out.println(" Encryption: " + enc.encrypt(1));
     }
 
     public static void case2() {
@@ -39,7 +49,7 @@ public class MainClass {
         System.out.print("\n~Enter text to decrypt: ");
         String x=  scan.nextLine();
         enc.setInput(x);
-        System.out.println("Decryption:" + enc.encrypt(2));
+        System.out.println(" Decryption:" + enc.encrypt(2));
     }
 
     public static void case3() {
@@ -52,5 +62,27 @@ public class MainClass {
         Scanner scan = new Scanner(System.in);
         System.out.print("\n~Enter the value per shift: ");
         enc.setS(scan.nextInt());
+    }
+
+    public static void case5() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\n~Enter the keyword: ");
+        String y=  scan.nextLine();
+        enc.setKey(y);
+        System.out.print("\n~Enter text to encrypt: ");
+        String x=  scan.nextLine();
+        enc.setInput(x);
+        System.out.println(" Encryption:" + enc.encryptKey(1));
+    }
+
+    public static void case6() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\n~Enter the keyword: ");
+        String y=  scan.nextLine();
+        enc.setKey(y);
+        System.out.print("\n~Enter text to decrypt: ");
+        String x=  scan.nextLine();
+        enc.setInput(x);
+        System.out.println(" Decryption:" + enc.encryptKey(2));
     }
 }
